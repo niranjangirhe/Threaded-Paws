6a526d020085ef0b5bc863b34ee71038cf899a35
Riddhi More <more.riddhi11@gmail.com>
2022-07-27 15:35:17 -0400

    added pause screen to all levels

--- src/Assets/scripts/GameManager.cs
+++ src/Assets/scripts/GameManager.cs
@@ -9,15 +9,27 @@ public class GameManager : MonoBehaviour
     public GameObject pauseScreenPrefab;
     private GameObject pauseScreen;
     private Transform Canvas;
+    private AudioSource[] allAudioSources;
 
     // Start is called before the first frame update
     void Start()
     {
+        //make instance of pause screen
         pauseScreen = Instantiate(pauseScreenPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        
         Canvas = GameObject.Find("Canvas").transform;
+        
+        //set parent as canvas for pause screen
         pauseScreen.transform.position = pauseScreenPrefab.transform.position;
         pauseScreen.transform.SetParent(Canvas, false);
+
+
         pauseScreen.SetActive(false);
+
+        //get all audio sources
+        allAudioSources = FindObjectsOfType(typeof(AudioSource)) as AudioSource[];
+
+        //start game
         isGameActive = true;
         isGamePaused = false;
         Time.timeScale = 1f;
@@ -29,7 +41,7 @@ public class GameManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            ChangePause();
+            ChangePause(); 
         }
     }
 
@@ -38,17 +50,26 @@ public class GameManager : MonoBehaviour
 
         if (isGameActive)
         {
+            //if game is not paused
             if (!isGamePaused)
             {
                 Time.timeScale = 0.0f;
                 isGamePaused = true;
                 pauseScreen.SetActive(true);
+                foreach (AudioSource audioS in allAudioSources)
+                {
+                    audioS.Pause();
+                }
             }
             else
             {
                 Time.timeScale = 1.0f;
                 isGamePaused = false;
                 pauseScreen.SetActive(false);
+                foreach (AudioSource audioS in allAudioSources)
+                {
+                    audioS.Play(0);
+                }
             }
         }
     }
