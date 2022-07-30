13df8a2f0a6646bc46fe32b4a91133b4d3d0d9ec
niranjangirhe <niranjangirheindia@gmail.com>
2022-07-30 00:36:28 -0400

    Tutorial check added

--- src/Assets/scripts/EventRecorded.cs
+++ src/Assets/scripts/EventRecorded.cs
@@ -10,12 +10,20 @@ public class EventRecorded : MonoBehaviour
     ExecuteThreadsLevel exe;
     [SerializeField] private int TutLevel;
     [SerializeField] private Animator animator;
-    
+    public List<BlockInfo> validSeq;
+    private List<bool> flags;
+
     // Start is called before the first frame update
     void Start()
     {
         exe = gameObject.GetComponent<ExecuteThreadsLevel>();
         threads = exe.threads;
+        switch(TutLevel)
+        {
+            case 1: animator.Play("Tut1S1"); break;
+            case 2: animator.Play("Tut2S1"); break;
+        }
+        
         StartCoroutine(checkoneBlock(0));
     }
 
@@ -40,10 +48,127 @@ public class EventRecorded : MonoBehaviour
             }
         }
         int count = 0;
+        flags = new List<bool>();
         foreach (Thread t in threads)
         {
             foreach (Transform child in exe.GetActionBlocks(t.tabDropArea))
             {
+                
+                switch (validSeq[count].actionEnum)
+                {
+                    case ActionEnum.CheckIn:
+                        flags.Add(child.name == "CheckInBox");
+                        break;
+                    case ActionEnum.CheckOut:
+                        flags.Add(child.name == "CheckOutBox");
+                        break;
+                    case ActionEnum.Cut:
+                        flags.Add(child.name == "CutBox");
+                        break;
+                    case ActionEnum.Dry:
+                        flags.Add(child.name == "DryBox");  
+                        break;
+                    case ActionEnum.Get:
+                        flags.Add(child.name == "ResourceBox");
+                        switch (validSeq[count].itemsEnum)
+                        {
+                            case ItemsEnum.Null:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "[null]";
+                                box.gameObject.transform.Find("Label").GetComponent<Text>().color = Color.red;
+                                break;
+                            case ItemsEnum.brush:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "brush";
+                                break;
+                            case ItemsEnum.clippers:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "clippers";
+                                break;
+                            case ItemsEnum.conditioner:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "cond.";
+                                break;
+                            case ItemsEnum.dryer:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "dryer";
+                                break;
+                            case ItemsEnum.scissors:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "scissors";
+                                break;
+                            case ItemsEnum.shampoo:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "shampoo";
+                                break;
+                            case ItemsEnum.spray:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "spray";
+                                break;
+                            case ItemsEnum.towel:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "towel";
+                                break;
+                            case ItemsEnum.sponge:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "sponge";
+                                break;
+                            case ItemsEnum.cash_reg:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "cash reg.";
+                                break;
+                        }
+
+                        break;
+                    case ActionEnum.Brush:
+                        newBox(actionPrefab, "brush", t.tabDropArea, "BrushBox");
+                        break;
+                    case ActionEnum.Return:
+                        box = newBox(returnPrefab, "ret", t.tabDropArea, "ReturnBox");
+
+                        switch (validSeq[count].itemsEnum)
+                        {
+                            case ItemsEnum.Null:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "[null]";
+                                box.gameObject.transform.Find("Label").GetComponent<Text>().color = Color.red;
+                                break;
+                            case ItemsEnum.brush:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "brush";
+                                break;
+                            case ItemsEnum.clippers:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "clippers";
+                                break;
+                            case ItemsEnum.conditioner:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "cond.";
+                                break;
+                            case ItemsEnum.dryer:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "dryer";
+                                break;
+                            case ItemsEnum.scissors:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "scissors";
+                                break;
+                            case ItemsEnum.shampoo:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "shampoo";
+                                break;
+                            case ItemsEnum.spray:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "spray";
+                                break;
+                            case ItemsEnum.towel:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "towel";
+                                break;
+                            case ItemsEnum.sponge:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "sponge";
+                                break;
+                            case ItemsEnum.cash_reg:
+                                box.gameObject.transform.Find("Dropdown").GetComponent<dropDownManager>().selected = "cash reg.";
+                                break;
+                        }
+                        break;
+                    case ActionEnum.Wash:
+                        newBox(actionPrefab, "wash", t.tabDropArea, "WashBox");
+                        break;
+                    case ActionEnum.Read:
+                        if (isDataRace)
+                            newBox(dataPrefab, "read", t.tabDropArea, "ReadBox");
+                        break;
+                    case ActionEnum.Calculate:
+                        if (isDataRace)
+                            newBox(dataPrefab, "calculate", t.tabDropArea, "CalculateBox");
+                        break;
+                    case ActionEnum.Write:
+                        if (isDataRace)
+                            newBox(dataPrefab, "write", t.tabDropArea, "WriteBox");
+                        break;
+                }
                 count++;
             }
         }
