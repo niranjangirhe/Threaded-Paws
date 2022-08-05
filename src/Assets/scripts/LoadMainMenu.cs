869c9ae94c808d3ab709dafddba6925b2760af5c
Riddhi More <more.riddhi11@gmail.com>
2022-08-05 18:53:17 -0400

    added audio mute and unmute in pause

--- src/Assets/scripts/LoadMainMenu.cs
+++ src/Assets/scripts/LoadMainMenu.cs
@@ -2,12 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadMainMenu : MonoBehaviour
 {
-	public void MainMenu()
+    private GameObject muteMusicSprite;
+    private GameObject unmuteMusicSprite;
+    private AudioSource music;
+    private void Start()
+    {
+        music = GameObject.Find("Logging").GetComponent<AudioSource>();
+        unmuteMusicSprite = GameObject.Find("On");
+        muteMusicSprite = GameObject.Find("Off");
+    }
+    public void MainMenu()
 	{
 		
 		SceneManager.LoadScene("MainMenu");
 	}
-}
+
+    public void MuteAudio()
+    {
+
+        music.mute = true;
+        muteMusicSprite.SetActive(true);
+        unmuteMusicSprite.SetActive(false);
+
+    }
+    public void UnmuteAudio()
+    {
+        music.mute = false;
+        muteMusicSprite.SetActive(false);
+        unmuteMusicSprite.SetActive(true);
+    }
+   }
