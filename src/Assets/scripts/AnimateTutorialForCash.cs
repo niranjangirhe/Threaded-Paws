5853d867a7587f7924d58ba993bf1e2693860755
Riddhi More <more.riddhi11@gmail.com>
2022-08-15 00:36:26 -0400

    added animation to redirect to next level after end

--- src/Assets/scripts/AnimateTutorialForCash.cs
+++ src/Assets/scripts/AnimateTutorialForCash.cs
@@ -5,59 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class AnimateTutorialForCash : MonoBehaviour
 {
-    [SerializeField] private Animator exeAnimator;
-    [SerializeField] private Animator inAnimator;
-    [SerializeField] private AnimationClip exeAnim;
-    [SerializeField] private AnimationClip inAnim;
-    [SerializeField] private GameObject exeObj;
-    [SerializeField] private GameObject inObj;
-    AudioSource bgSong;
-    LogManager lm;
+
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        lm =  GameObject.Find("Logging").GetComponent<LogManager>();
-        lm.tempUnlocked = false;
-        Cursor.lockState = CursorLockMode.Locked;
-        StartCoroutine(AnimateS1(0f));
-        bgSong = GameObject.Find("Logging").GetComponent<AudioSource>();
+        
     }
 
     private void Update()
     {
-       ExecuteThreadsLevel.ReduceSound(bgSong, 0.1f, 0.5f);
-        if (lm.tempUnlocked)
+        if (this.transform.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime >1.0f)
         {
-            Cursor.lockState = CursorLockMode.None;
+           
+            
+            SceneManager.LoadScene("Level 5");
         }
-        else
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-        }
-    }
-
-    IEnumerator AnimateS1(float sec)
-    {
-        yield return new WaitForSeconds(sec);
-        exeObj.SetActive(true);
-        inObj.SetActive(false);
-        exeAnimator.Play("ForeGroundMover");
-        StartCoroutine(AnimateS2(exeAnim.length));
-        //StartCoroutine(LoadScene(exeAnim.length));
-    }
-    IEnumerator AnimateS2(float sec)
-    {
-        yield return new WaitForSeconds(sec);
-        inObj.SetActive(true);
-        exeObj.SetActive(false);
-        inAnimator.Play("Interior");
-        StartCoroutine(LoadScene(inAnim.length));
-    }
-    IEnumerator LoadScene(float sec)
-    {
-        yield return new WaitForSeconds(sec);
-        Cursor.lockState = CursorLockMode.None;
-        SceneManager.LoadScene("Tut 1");
     }
 }
+  
+   
