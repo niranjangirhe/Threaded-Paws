3d6dc2cd05d8121455f5340fae1aa34a08f4ad93
Riddhi More <more.riddhi11@gmail.com>
2022-08-16 05:46:42 -0400

    added voice lines to tutorial level 4 video clip

--- src/Assets/scripts/AnimateTutorialForCash.cs
+++ src/Assets/scripts/AnimateTutorialForCash.cs
@@ -5,16 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class AnimateTutorialForCash : MonoBehaviour
 {
-
+    AudioSource bgSong;
 
     // Start is called before the first frame update
     private void Start()
     {
-        
+        bgSong = GameObject.Find("Logging").GetComponent<AudioSource>();
     }
 
     private void Update()
     {
+        ExecuteThreadsLevel.ReduceSound(bgSong, 0.1f, 0.5f);
         //check if animation is over
         if (this.transform.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).normalizedTime >1.0f)
         {
